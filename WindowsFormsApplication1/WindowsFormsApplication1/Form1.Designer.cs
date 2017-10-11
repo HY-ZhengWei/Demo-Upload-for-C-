035f17e6cdd7219ace1b8107b8bfec580e0524c7
HY-ZhengWei <hy.zhengwei@qq.com>
2017-10-11 17:44:38 +0800

    1.0.0

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.selectUploadFile = new System.Windows.Forms.Button();
+            this.upload = new System.Windows.Forms.Button();
+            this.showFile = new System.Windows.Forms.TextBox();
+            this.uploadProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // selectUploadFile
+            // 
+            this.selectUploadFile.Location = new System.Drawing.Point(617, 28);
+            this.selectUploadFile.Name = "selectUploadFile";
+            this.selectUploadFile.Size = new System.Drawing.Size(100, 23);
+            this.selectUploadFile.TabIndex = 0;
+            this.selectUploadFile.Text = "选择上传的文件";
+            this.selectUploadFile.UseVisualStyleBackColor = true;
+            this.selectUploadFile.Click += new System.EventHandler(this.selectUploadFile_Click);
+            // 
+            // upload
+            // 
+            this.upload.Enabled = false;
+            this.upload.Location = new System.Drawing.Point(617, 75);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(100, 23);
+            this.upload.TabIndex = 1;
+            this.upload.Text = "开始上传";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.upload_Click);
+            // 
+            // showFile
+            // 
+            this.showFile.Location = new System.Drawing.Point(12, 28);
+            this.showFile.Name = "showFile";
+            this.showFile.ReadOnly = true;
+            this.showFile.Size = new System.Drawing.Size(599, 21);
+            this.showFile.TabIndex = 2;
+            // 
+            // uploadProgress
+            // 
+            this.uploadProgress.Location = new System.Drawing.Point(12, 75);
+            this.uploadProgress.Name = "uploadProgress";
+            this.uploadProgress.Size = new System.Drawing.Size(599, 23);
+            this.uploadProgress.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 126);
+            this.Controls.Add(this.uploadProgress);
+            this.Controls.Add(this.showFile);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.selectUploadFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "断点续传的上传演示";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button selectUploadFile;
+        private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.TextBox showFile;
+        private System.Windows.Forms.ProgressBar uploadProgress;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void selectUploadFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Excel文件(*.xls;*.xlsx)|*.xls;*.xlsx|所有文件|*.*";
+            openFileDialog1.ValidateNames   = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            this.showFile.Text  = openFileDialog1.FileName;
+            this.upload.Enabled = true;
+        }
+
+        private void upload_Click(object sender, EventArgs e)
+        {
+            this.selectUploadFile.Enabled = false;
+            this.upload.Enabled = false;
+            
+            FileInfo   v_File  = null;
+            FileStream v_Input = null;
+
+            try
+            {
+                v_File  = new FileInfo(this.showFile.Text);
+                v_Input = new FileStream(this.showFile.Text, FileMode.Open);
+
+                int    v_DataNo    = 0;
+                byte[] v_Data      = new byte[1024 * 100];
+                double v_DataCount = (double)v_File.Length / (double)v_Data.Length;
+                string v_FileName  = DateTime.Now.ToFileTimeUtc().ToString() + v_File.Extension;
+
+                v_DataCount = Math.Ceiling(v_DataCount);
+
+                this.uploadProgress.Value   = 0;
+                this.uploadProgress.Minimum = 0;
+                this.uploadProgress.Maximum = (int)v_DataCount;
+
+                while (true)
+                {
+                    int v_DataLen = v_Input.Read(v_Data, 0, v_Data.Length);
+                    if (v_DataLen == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        v_DataNo++;
+                        string v_SendData = v_FileName
+                                          + ";" + v_DataCount
+                                          + ";" + v_DataNo
+                                          + ";" + ByteHelp.bytesToHex(v_Data ,0 , v_DataLen)
+                                          + ";" + "new"
+                                          + ";" + "用户名称";
+                        sendHttpPost("http://127.0.0.1:80/xx/hoto", v_SendData);
+                        this.uploadProgress.Value = v_DataNo;
+                    }
+                }
+            }
+            catch (Exception exce)
+            {
+                Console.WriteLine("{0} Second exception.", exce.Message);
+            }
+            finally
+            {
+                if (v_Input != null)
+                {
+                    v_Input.Close();
+                }
+            }
+
+
+            this.selectUploadFile.Enabled = true;
+            this.upload.Enabled = true;
+            MessageBox.Show("上传完成");
+        }
+
+
+
+        public static string sendHttpPost(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = postDataStr.Length;
+            StreamWriter writer = new StreamWriter(request.GetRequestStream(), Encoding.ASCII);
+            writer.Write(postDataStr);
+            writer.Flush();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string encoding = response.ContentEncoding;
+            if (encoding == null || encoding.Length < 1)
+            {
+                encoding = "UTF-8"; //默认编码 
+            }
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+            string retString = reader.ReadToEnd();
+            return retString;
+        }
+
+    }
+}
